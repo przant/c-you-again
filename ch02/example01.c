2bf0cac223a819a015bdac17f501d0eaa1f6455c
Antonio Pérez <apema2079@gmail.com>
2022-04-08 22:46:55 -0500

    Add First example

---
+++ ch02/example01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+   // variable declarations
+   int x;
+   float y;
+   char c;
+
+   // variable initializations
+   x = -4443;
+   y = 554.21;
+   c = 'M';
+
+   // printing variable contents to standard output
+   printf("\nThe value of integer variable x is %d\n", x);
+   printf("\nThe value of float variable y is %f", y);
+   printf("\nThe value of character variable c is %c", c);
+
+   return 0;
+}
