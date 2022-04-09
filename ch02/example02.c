04dd61d642a142f9b1cb71566f698f0de0153420
Antonio Pérez <apema2079@gmail.com>
2022-04-08 22:49:09 -0500

    Add second example

---
+++ ch02/example02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+   const int x = 20;
+   const float PI = 3.14;
+
+   printf("Constant values are %d and %.2f\n", x, PI);
+
+   return 0;
+}
