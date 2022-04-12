43e451dca7877da2cc530b5e450f0ac10ad02b91
Antonio Pérez <apema2079@gmail.com>
2022-04-12 06:24:40 -0500

    Add first example

---
+++ ch04/example01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[]) {
+   int x = 0;
+   int y = 0;
+
+   printf("\nThe value of y is %d\n", y++);
+   printf("\nThe value of x is %d\n", ++x);
+
+   return 0;
+}
